fe976c86ff1e303f785a3fb136abfdcf0274b1b8
Teri Bernard <teri@bernardtechpartners.com>
2017-12-31 22:06:46 -0500

    Set up add-client component with form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 // Connect to Firebase Database
 import { AngularFireModule } from 'angularfire2';
@@ -27,7 +28,8 @@ import { ClientService } from './services/client.service';
 const appRoutes = [
   { path: '', component: DashboardComponent },
   { path: 'register', component: RegisterComponent},
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'add-client', component: AddClientComponent }
 ];
 
 @NgModule({
@@ -47,6 +49,7 @@ const appRoutes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes),
     AngularFireModule.initializeApp( environment.firebase, 'clientpanel'),
     AngularFireAuthModule
